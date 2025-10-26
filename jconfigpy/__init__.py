3048b14a0001e252a06da1fd2c125ef80ecc5a8e
fritzprix <innocentevil0914@gmail.com>
2025-10-26 17:14:20 +0900

    fix: Update __init__.py with absolute/relative import fallback

--- jconfigpy/__init__.py
+++ jconfigpy/__init__.py
@@ -1,6 +1,13 @@
-from Dialog import Dialog
-from Dialog import CMDDialog
-from ErrorType import FileNotExistError
-from VariableMonitor import Monitor
-from Config import JConfig
+try:
+    from jconfigpy.Dialog import Dialog
+    from jconfigpy.Dialog import CMDDialog
+    from jconfigpy.ErrorType import FileNotExistError
+    from jconfigpy.VariableMonitor import Monitor
+    from jconfigpy.Config import JConfig
+except ImportError:
+    from .Dialog import Dialog
+    from .Dialog import CMDDialog
+    from .ErrorType import FileNotExistError
+    from .VariableMonitor import Monitor
+    from .Config import JConfig
 
