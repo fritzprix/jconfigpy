c45cd53486de76546984d9997da3f8342a523998
fritzprix <innocentevil0914@gmail.com>
2025-10-26 17:13:00 +0900

    chore: Bump version to 0.2.1 and modernize Dockerfile
    - Updated version from 0.2.0 to 0.2.1
    - Modernized Dockerfile with Python 3 explicit support
    - Updated CMD to use python3 -m jconfigpy for proper module execution
    - Added pip3 install for package installation

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,12 @@ FROM	fritzprix/cbuild:0.0.1
 
 MAINTAINER	fritzprix
 
-RUN	apt-get update
-RUN	git clone https://github.com/fritzprix/jconfigpy.git
-CMD python jconfigpy -s -i ./configs/config -t ./example/config.json
+RUN	apt-get update && apt-get install -y python3 python3-pip
+
+WORKDIR	/jconfigpy
+
+RUN	git clone https://github.com/fritzprix/jconfigpy.git /jconfigpy
+
+RUN	pip3 install -e .
+
+CMD	["python3", "-m", "jconfigpy", "-s", "-i", "./configs/config", "-t", "./example/config.json"]
--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ def read(fname):
 
 setup(
     name="jconfigpy",
-    version="0.2.0",
+    version="0.2.1",
     author="fritzprix",
     author_email="innocentevil0914@gmail.com",
     description="configuration utility which easily integrated into project using gnu make as build system",
     license="MIT",
     keywords="configuration utility make",
     url="http://github.com/fritzprix/jconfigpy",
-    download_url="http://github.com/fritzprix/jconfigpy/archive/0.2.0.tar.gz",
+    download_url="http://github.com/fritzprix/jconfigpy/archive/0.2.1.tar.gz",
     packages=['jconfigpy'],
     python_requires='>=3.6',
     long_description=read('README.md'),
